c68ae5f133712bb9d64ed5ddedf00677923605ba
Joachim Løvgaard <joachim@loevgaard.dk>
2025-05-09 08:18:45 +0200

    Simplify Twig test

--- tests/Twig/ExtensionTest.php
+++ tests/Twig/ExtensionTest.php
@@ -6,38 +6,22 @@ namespace Setono\SyliusToggleVatPlugin\Tests\Twig;
 
 use Prophecy\PhpUnit\ProphecyTrait;
 use Setono\SyliusToggleVatPlugin\Context\DefaultVatContext;
-use Setono\SyliusToggleVatPlugin\Context\VatContextInterface;
 use Setono\SyliusToggleVatPlugin\Twig\Extension;
 use Setono\SyliusToggleVatPlugin\Twig\Runtime;
-use Twig\RuntimeLoader\RuntimeLoaderInterface;
+use Twig\RuntimeLoader\FactoryRuntimeLoader;
 use Twig\Test\IntegrationTestCase;
-use Webmozart\Assert\Assert;
 
 final class ExtensionTest extends IntegrationTestCase
 {
     use ProphecyTrait;
 
-    public function getRuntimeLoaders(): array
+    public function getRuntimeLoaders(): \Generator
     {
-        $vatContext = new DefaultVatContext(true);
-
-        $runtimeLoader = new class($vatContext) implements RuntimeLoaderInterface {
-            public function __construct(private readonly VatContextInterface $vatContext)
-            {
-            }
-
-            /**
-             * @param string $class
-             */
-            public function load($class): Runtime
-            {
-                Assert::same($class, Runtime::class);
-
-                return new Runtime($this->vatContext);
-            }
-        };
-
-        return [$runtimeLoader];
+        yield new FactoryRuntimeLoader([
+            Runtime::class => static function (): Runtime {
+                return new Runtime(new DefaultVatContext(true));
+            },
+        ]);
     }
 
     public function getExtensions(): array
